d13781c9313ab1a378072b5a4f5ebf18883ae4b6
2580isaack <125550217+2580isaack@users.noreply.github.com>
2025-10-25 16:11:58 +0300

    Update Project03_app.py

--- Project03_app.py
+++ Project03_app.py
@@ -558,13 +558,13 @@ if st.session_state.logged_in:
              st.markdown("---")
              st.header("🧠 Select a Security Module")
              cols = st.columns(3)
-for i, mod in enumerate(MODULE_FUNCTIONS.keys()):
-        with cols[i % 3]:
-            if st.button(mod, key=f"mod_{mod}"):
-                st.session_state.page = mod
-                st.rerun()
-        st.header("Select a Module")
-cols = st.columns(3)
+        for i, mod in enumerate(MODULE_FUNCTIONS.keys()):
+            with cols[i % 3]:
+                if st.button(mod, key=f"mod_{mod}"):
+                    st.session_state.page = mod
+                    st.rerun()
+                    st.header("Select a Module")
+                    cols = st.columns(3)
 for i, mod in enumerate(MODULE_FUNCTIONS.keys()):
     with cols[i % 3]:
         if st.button(mod, key=f"mod_{mod}"):
